057595d1ec7c63dcf7a86a3ba81d9e997fcd1b17
angelkapv81@gmail.com <angelkapv81>
2023-05-18 22:29:29 +0300

    fix eslint warn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,13 @@ import Contacts from './Contacts/Contacts';
 import swal from 'sweetalert';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState([]);
 
   // const [name, setName] = useState('');
   const [filter, setFilter] = useState('');
 
   // перше завантаження
+
   useEffect(() => {
     const localData = localStorage.getItem('contacts');
     if (localData) {
@@ -25,22 +21,12 @@ function App() {
     } else {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // КОЛИ ОНОВЛЮЄТЬСЯ ДОМ
-
-  // useEffect(() => {
-  //   const localData = localStorage.getItem('contacts');
-  //   if (localData) {
-  //     setContacts(JSON.parse(localData));
-  //   } else {
-  //     localStorage.setItem('contacts', JSON.stringify(contacts));
-  //   }
-  // }, [contacts]);
 
   const addContact = (name, number) => {
     setFilter('');
